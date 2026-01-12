629e72dd497922cab050fa226393f7e7684253c1
aleksfallenarsentev1932-coder <aleksfallenarsentev1932@gmail.com>
2026-01-12 17:48:19 +0300

    шаг5
    add skeleton app

---
+++ DisctrictPortal/DisctrictPortal/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DisctrictPortal
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("TaskTracker v0.1");
+            Console.WriteLine("----------------");
+            Console.WriteLine("1) Добавить задачу (пока заглушка)");
+            Console.WriteLine("2) Показать список задач (пока заглушка)");
+            Console.WriteLine("3) Изменить статус задачи (пока заглушка)");
+            Console.WriteLine("4) Удалить задачу (пока заглушка)");
+            Console.WriteLine("0) Выход");
+            Console.WriteLine("----------------");
+            Console.Write("Выберите пункт меню: ");
+
+            var input = Console.ReadLine();
+            Console.WriteLine($"Вы выбрали: {input}");
+            Console.WriteLine("Пока это только каркас. Логика появится в следующих ЛР.");
+        }
+    }
+}
---
+++ DisctrictPortal/DisctrictPortal/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов для изменения сведений,
+// связанные с этой сборкой.
+[assembly: AssemblyTitle("DisctrictPortal")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("DisctrictPortal")]
+[assembly: AssemblyCopyright("Copyright ©  2026")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// из модели COM задайте для атрибута ComVisible этого типа значение true.
+[assembly: ComVisible(false)]
+
+// Следующий GUID представляет идентификатор typelib, если этот проект доступен из модели COM
+[assembly: Guid("c9b41986-2a95-4708-8df8-678308436b42")]
+
+// Сведения о версии сборки состоят из указанных ниже четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//      Номер сборки
+//      Номер редакции
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
