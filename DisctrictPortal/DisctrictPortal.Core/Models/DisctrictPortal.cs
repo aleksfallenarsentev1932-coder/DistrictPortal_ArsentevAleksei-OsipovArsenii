bffbf40a2138011ccd4a7000668cfac87ec6b5a6
Anillink <iskhakova.a@internet.ru>
2026-01-16 14:47:52 +0300

    feat: add Core project

---
+++ DisctrictPortal/DisctrictPortal.Core/Models/DisctrictPortal.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DisctrictPortal.Core.Models
+{
+    public class Post
+    {
+        public int Id { get; set; } 
+        public string Title { get; set; } = ""; 
+        public string Content { get; set; } = ""; 
+        public string? ImageUrl { get; set; } 
+        public string Address { get; set; } = "";
+        public DateTime CreatedAt { get; set; }
+
+        public PostCategory Category { get; set; } 
+        public string AuthorId { get; set; } = ""; 
+        public User? Author { get; set; } 
+
+        public List<Comment> Comments { get; set; } = new();
+    }
+
+    public enum PostCategory
+    {
+        News,       
+        Problem,     
+        Sale,        
+        Service,     
+        LostAndFound 
+    }
+
+    public class User
+    {
+        public string Id { get; set; } = "";
+        public string Name { get; set; } = "";
+        public string Phone { get; set; } = "";
+        public string Address { get; set; } = ""; 
+
+        public List<Post> Posts { get; set; } = new();
+        public List<Comment> Comments { get; set; } = new();
+    }
+
+    public class Comment
+    {
+        public int Id { get; set; }
+        public int PostId { get; set; }
+        public string Text { get; set; } = "";
+        public string AuthorId { get; set; } = "";
+        public DateTime CreatedAt { get; set; }
+
+        public Post? Post { get; set; } 
+        public User? Author { get; set; } 
+    }
+
+    public class MapMarker
+    {
+        public int Id { get; set; }
+        public double Latitude { get; set; }
+        public double Longitude { get; set; }
+        public string Description { get; set; } = "";
+        public string? PhotoUrl { get; set; }
+
+        public MarkerStatus Status { get; set; } 
+    }
+
+    public enum MarkerStatus
+    {
+        New,       
+        InProgress, 
+        Resolved    
+    }
+
+}
+
--- DisctrictPortal/DisctrictPortal/Program.cs
+++ DisctrictPortal/DisctrictPortal/Program.cs
@@ -10,19 +10,19 @@ namespace DisctrictPortal
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("TaskTracker v0.1");
+            Console.WriteLine("DisctrictPortal");
             Console.WriteLine("----------------");
-            Console.WriteLine("1) Добавить задачу (пока заглушка)");
-            Console.WriteLine("2) Показать список задач (пока заглушка)");
-            Console.WriteLine("3) Изменить статус задачи (пока заглушка)");
-            Console.WriteLine("4) Удалить задачу (пока заглушка)");
+            Console.WriteLine("1) Добавить пост(пока заглушка)");
+            Console.WriteLine("2) Редактировать пост(пока заглушка)");
+            Console.WriteLine("3) Изменить статус поста(пока заглушка)");
+            Console.WriteLine("4) Удалить пост(пока заглушка)");
             Console.WriteLine("0) Выход");
             Console.WriteLine("----------------");
             Console.Write("Выберите пункт меню: ");
 
             var input = Console.ReadLine();
             Console.WriteLine($"Вы выбрали: {input}");
-            Console.WriteLine("Пока это только каркас. Логика появится в следующих ЛР.");
+            
         }
     }
 }
